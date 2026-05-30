88ea07f7155e4958d90c5d339d5fa288d7a275b6
Zohara Duenias <zohara.duenias@gmail.com>
2026-05-31 01:37:58 +0300

    chore: remove boilerplate comments

--- ImageProcessing.Api/ImageProcessing.Api/Program.cs
+++ ImageProcessing.Api/ImageProcessing.Api/Program.cs
@@ -4,22 +4,17 @@ using ImageProcessing.Services.Stores;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Application services
 builder.Services.AddSingleton<IImageStore, InMemoryImageStore>();
 builder.Services.AddScoped<IImageService, ImageService>();
 builder.Services.AddScoped<IImageProcessingService, ImageProcessingService>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
